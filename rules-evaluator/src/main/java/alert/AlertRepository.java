5d8ffca54cedd58b97be0e58b68fc040ab19a8e2
saleh sabagh <saleh4532eh@gmail.com>
2026-07-23 02:59:03 +0330

    refactor(AlertRepository) : change alert's list name and delete unnecessary comments

--- rules-evaluator/src/main/java/alert/AlertRepository.java
+++ rules-evaluator/src/main/java/alert/AlertRepository.java
@@ -4,17 +4,7 @@ import java.util.List;
 
 public interface AlertRepository {
 
-    /**
-     * Saves a new alert into the database.
-     *
-     * @param alert the alert object to save
-     */
     void save(Alert alert);
 
-    /**
-     * Retrieves all stored alerts, typically ordered by creation time.
-     *
-     * @return a list of all alerts
-     */
-    List<Alert> findAllOrderedByTime();
+    List<Alert> findAllOrderByCreatedAt();
 }
