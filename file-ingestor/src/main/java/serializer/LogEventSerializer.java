d0fdd5208ec3f2e46cd0bc21a97ab0f6f4457303
saleh sabagh <saleh4532eh@gmail.com>
2026-07-23 16:20:09 +0330

    fix(serializer): register JavaTimeModule in LogEventDeserializer

--- file-ingestor/src/main/java/serializer/LogEventSerializer.java
+++ file-ingestor/src/main/java/serializer/LogEventSerializer.java
@@ -4,11 +4,11 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import model.LogEvent;
 import org.apache.kafka.common.serialization.Serializer;
-
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 public class LogEventSerializer implements Serializer<LogEvent> {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper()
+            .registerModule(new JavaTimeModule());
     @Override
     public byte[] serialize(String topic, LogEvent data) {
         if (data == null) {
@@ -16,7 +16,7 @@ public class LogEventSerializer implements Serializer<LogEvent> {
         }
 
         try {
-            return objectMapper.writeValueAsBytes(data);
+            return OBJECT_MAPPER.writeValueAsBytes(data);
         } catch (JsonProcessingException e) {
             throw new RuntimeException("Failed to serialize LogEvent", e);
         }
