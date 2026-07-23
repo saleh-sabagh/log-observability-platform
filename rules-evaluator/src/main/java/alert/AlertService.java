0208f1e40448a12c2ceef922f2417d29b6212e45
saleh sabagh <saleh4532eh@gmail.com>
2026-07-24 02:32:14 +0330

    refactor(alert): delegate alert creation logic to AlertService

--- rules-evaluator/src/main/java/alert/AlertService.java
+++ rules-evaluator/src/main/java/alert/AlertService.java
@@ -1,8 +1,12 @@
 package alert;
 
+import model.LogEvent;
+import rule.RuleDefinition;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class AlertService {
 
@@ -16,15 +20,15 @@ public class AlertService {
     }
 
     public void createAlert(
-            String ruleName,
-            String component,
-            String description
+            RuleDefinition rule,
+            LogEvent triggeringEvent,
+            List<LogEvent> matchedEvents
     ) {
 
         Alert alert = Alert.builder()
-                .ruleName(ruleName)
-                .component(component)
-                .description(description)
+                .ruleName(rule.getRuleName())
+                .component(triggeringEvent.getComponent())
+                .description(buildDescription(rule, matchedEvents))
                 .createdAt(LocalDateTime.now())
                 .build();
 
@@ -33,5 +37,23 @@ public class AlertService {
 
     public List<Alert> getAllAlerts() {
         return alertRepository.findAllOrderedByCreatedAt();
+    }
+
+    private String buildDescription(
+            RuleDefinition rule,
+            List<LogEvent> matchedEvents
+    ) {
+
+        String messages = matchedEvents.stream()
+                .limit(rule.getMaxLastMessages())
+                .map(LogEvent::getMessage)
+                .collect(Collectors.joining(" | "));
+
+        return String.format(
+                "Rule '%s' triggered with %d matching event(s). Recent messages: %s",
+                rule.getRuleName(),
+                matchedEvents.size(),
+                messages
+        );
     }
 }
